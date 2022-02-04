e7aa8d7481e0e2003085c4170e6b30c5ee413b12
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-02-04 07:18:45 -0800

    Completed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,44 @@ function App() {
     email: ""
   });
 
+  function onChangeHandler(event) {
+    console.log("onChangeHanlder()...");
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+    console.log(inputName + "=" + newValue);
+    const {
+      value,
+      name
+    } = event.target; /* value is the same as newName , name is the same as inputName*/
+    console.log(name + "=" + value);
+
+    setContact((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  function onClickHandler(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,10 +52,25 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
-        <button>Submit</button>
+        <input
+          onChange={onChangeHandler}
+          value={contact.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={onChangeHandler}
+          value={contact.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={onChangeHandler}
+          value={contact.email}
+          name="email"
+          placeholder="Email"
+        />
+        <button onClick={onClickHandler}>Submit</button>
       </form>
     </div>
   );
